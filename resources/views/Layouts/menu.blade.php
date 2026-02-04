2e565e63f647aff3270d1d90eb196a0f2b9ecf0b
Sophy <simsophy168lucky@gmail.com>
2026-02-04 16:34:48 +0700

    Update menu.blade.php layout

--- resources/views/Layouts/menu.blade.php
+++ resources/views/Layouts/menu.blade.php
@@ -7,7 +7,7 @@
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ url('/') }}" class="brand-link">
-        <img src="{{ asset('logo.png') }}" class="brand-image img-circle elevation-3" style="opacity: .8" />
+        <img src="{{ asset('logo.jpg') }}" class="brand-image img-circle elevation-3" style="opacity: .8" />
         <span class="brand-text font-weight-light">My Stock</span>
     </a>
 
